1cf58507c31861b075d28b8b35ea2723b6e6401c
BodiaKol <kolodchal.pn@ucu.edu.ua>
2025-04-02 13:49:04 +0300

    Додав сторінки:Buy,Sell(0,1,2), але без логіки

---
+++ frontend/src/pages/Sell2Page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Sell2Page() {
+    return   (
+    <>
+        <div style={{width: '100%', height: '100%', position: 'relative', backgroundImage: 'url(https://placehold.co/1920x1080)'}}>
+            <div style={{width: 1920, height: 1080, left: 0, top: 0, position: 'absolute', background: 'rgba(217, 217, 217, 0.01)', backdropFilter: 'blur(10px)'}} />
+            <div data-property-1="Default" style={{width: 1920, height: 32, left: 0, top: 1048, position: 'absolute'}}>
+                <div style={{width: 1920, height: 32, left: 0, top: 0, position: 'absolute', background: '#39236B'}} />
+                <div style={{width: 269, height: 16.50, left: 826, top: 8, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#EDEDED', fontSize: 12, fontFamily: 'Nunito', fontWeight: '400', wordWrap: 'break-word'}}>2025. All rights reserved</div>
+            </div>
+            <div style={{left: 301, top: 519, position: 'absolute'}}></div>
+            <div data-property-1="default" style={{width: 129, height: 32, left: 128, top: 48, position: 'absolute'}}>
+                <div style={{width: 129, height: 32, left: 0, top: 0, position: 'absolute'}} />
+                <div data-size="16" style={{width: 16, height: 16, left: 8, top: 8, position: 'absolute', overflow: 'hidden'}}>
+                    <div style={{width: 9.33, height: 9.33, left: 3.33, top: 3.33, position: 'absolute', outline: '1.60px #EDEDED solid', outlineOffset: '-0.80px'}} />
+                </div>
+                <div style={{left: 32, top: 8, position: 'absolute', color: '#EDEDED', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', lineHeight: 16, wordWrap: 'break-word'}}>Back to site</div>
+            </div>
+            <div data-page="page3" style={{width: 552, height: 406, left: 684, top: 144, position: 'absolute'}}>
+                <div style={{width: 552, height: 406, left: 0, top: 0, position: 'absolute', background: '#EDEDED', borderRadius: 64}} />
+                <div style={{left: 165, top: 64, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#39236B', fontSize: 32, fontFamily: 'e-Ukraine', fontWeight: '700', wordWrap: 'break-word'}}>Sell your car</div>
+                <div style={{left: 64, top: 166, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#39236B', fontSize: 16, fontFamily: 'Nunito', fontWeight: '400', lineHeight: 24, letterSpacing: 0.16, wordWrap: 'break-word'}}>All right?</div>
+                <div style={{width: 76, height: 32, left: 64, top: 310, position: 'absolute'}}>
+                    <div style={{width: 76, height: 32, left: 76, top: 32, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left'}} />
+                    <div data-size="16" style={{width: 16, height: 16, left: 8, top: 8, position: 'absolute', overflow: 'hidden'}}>
+                        <div style={{width: 9.33, height: 9.33, left: 3.33, top: 3.33, position: 'absolute', outline: '1.60px #39236B solid', outlineOffset: '-0.80px'}} />
+                    </div>
+                    <div style={{left: 32, top: 4, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#39236B', fontSize: 16, fontFamily: 'Nunito', fontWeight: '400', lineHeight: 24, letterSpacing: 0.16, wordWrap: 'break-word'}}>Back</div>
+                </div>
+                <div data-property-1="default" style={{width: 424, height: 48, left: 64, top: 198, position: 'absolute'}}>
+                    <div style={{width: 424, height: 48, left: 0, top: 0, position: 'absolute', background: '#39236B', borderRadius: 64}} />
+                    <div style={{width: 300.71, height: 27, left: 61.65, top: 10.50, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#EDEDED', fontSize: 20, fontFamily: 'Nunito', fontWeight: '700', lineHeight: 30, letterSpacing: 0.20, wordWrap: 'break-word'}}>Sell Car</div>
+                </div>
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default Sell2Page;
