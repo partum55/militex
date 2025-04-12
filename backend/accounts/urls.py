144f0162e45308653203f1c5d937f3b7ea18a079
Nazar Mykhailyshchuk <partumyt@gmail.com>
2025-04-12 21:33:31 +0300

    12.04 commit temp 3

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import signup_view, login_view
-
+from .views import signup_view, login_view, CustomAuthToken
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('signup/', CustomAuthToken.as_view(), name='signup'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 from .serializers import UserSignupSerializer, UserLoginSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key})
 
 @api_view(["POST"])
 def signup_view(request):
