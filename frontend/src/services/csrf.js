3eede939531faa082791605c328e7a0060d57e0a
vovaw123 <volodymyr.yanishevskyy@gmail.com>
2025-04-24 15:55:19 +0300

    cleaned csrf.js

--- frontend/src/services/csrf.js
+++ frontend/src/services/csrf.js
@@ -1,13 +1,8 @@
-// services/csrf.js
 import api from './api';
 
-/**
- * Fetches a CSRF token from the server and sets it as a cookie
- * This should be called when the app initializes
- */
+
 const getCSRFToken = async () => {
   try {
-    // Try to get CSRF token
     await api.get('/csrf/');
     return true;
   } catch (error) {
